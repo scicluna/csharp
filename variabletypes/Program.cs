8d82016633ad81b56f5716562567b719ebb86fec
scicluna <88948700+scicluna@users.noreply.github.com>
2023-05-16 12:10:17 -0400

    variable types

---
+++ variabletypes/Program.cs
@@ -0,0 +1,11 @@
+﻿//Declare variables by their type (or var if used implicitly)
+// char userOption;
+// int gameScore;
+// decimal particlesPerMillion;
+// bool processedCustomer;
+
+var name = "Bob";
+var inboxNumber = 3;
+var temp = 34.40M;
+
+Console.Write($"Hello, {name}! You have {inboxNumber} in your inbox. The temperature is {temp} celsius.");
