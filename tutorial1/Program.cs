8c91f739f35d1780a3d3c1dae9e7bb4705f4ae25
scicluna <88948700+scicluna@users.noreply.github.com>
2023-05-16 12:01:06 -0400

    literal types csharp

---
+++ tutorial1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello"); // string literal
+Console.WriteLine('b'); // char literal
+Console.WriteLine(123); // int literal
+Console.WriteLine(0.25F); // float literal
+Console.WriteLine(12.39816m); // decimal literal
+Console.WriteLine(2.625); // double literal
+Console.WriteLine(true); // bool literal
+
